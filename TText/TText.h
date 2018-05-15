49663142cb0f77f7e5d09753e005a6f8fca349e2
Matt <mat3580@gmail.com>
2018-05-15 14:33:02 +0300

    1 version

---
+++ TText/TText.h
@@ -0,0 +1,85 @@
+#pragma once
+#include <iostream>
+#include <stack>
+#include <fstream>
+#include "FontColor.h"
+
+using namespace std;
+class TLink;
+class TText;
+
+struct TMem {
+	TLink *pFirst;
+	TLink *pFree;
+	TLink *pLast;
+};
+
+class TLink {
+public:
+	char str[80];
+	TLink *pNext, *pDown;
+	bool isFree;
+	static TMem mem;
+
+	TLink(char *_str = NULL, TLink *pN = NULL, TLink *pD = NULL);
+	~TLink() {}
+
+	void *operator new(size_t s);
+	void operator delete(void *p);
+
+	static void InitMem(size_t s);
+	static void MemClear(TText &txt);
+
+	static void PrintFree();
+	void SetStr(char *_str = "\0");
+};
+
+
+
+
+
+class TText {
+	TLink *pFirst, *pCurr;
+	stack<TLink *> st;
+	int level;
+public:
+	TText() {}
+	TText(TLink *first);
+
+	//TText operator=(TText &t);   // не закончено
+
+	TLink GetCurr();
+	TLink *GetpFirst();
+	void SetCurrFalse();
+
+	void GoNextLink();
+	void GoDownLink();
+	void GoPrevLink();
+
+	void InsNextLine(char *s);
+	void InsNextSection(char *s);
+	void InsDownLine(char *s);
+	void InsDownSection(char *s);
+
+	void DelNext();
+	void DelDown();
+
+	char *GetLine();
+	void SetLine(char *s);
+
+	TLink *ReadRec(ifstream& file);
+	void Read(char *fn);
+
+	void PrintText(TLink *tmp);
+	void Print();
+
+	void SaveText(TLink *tmp, ofstream& f);
+	void Save(char *name);
+
+	TLink *CopyRec(TLink *first);
+	TLink *Copy();
+
+	void Reset();
+	bool IsEnd();
+	void GoNext();
+};
---
+++ TText/main.cpp
@@ -0,0 +1,112 @@
+#include "TText.h"
+#include <Windows.h>
+#include <conio.h>
+
+void clrscr(void);
+void clreol(void);
+void gotoxy(int x, int y);
+
+TMem TLink::mem;
+int LinkCount = 100;
+
+int main() {
+	TLink::InitMem(LinkCount);
+
+	char fileName[] = "C:\\Users\\User\\Documents\\GitHub projects\\Lab6-Text_v2.0\\Source.txt";
+	TText text;
+	char s;
+	char buf[80];
+	bool readFlag = false;
+
+	cout << "i - info\n";
+
+	while (true) {
+		SetColor(ConsoleColor::LightGreen, ConsoleColor::Black);
+		cout << "user: ";
+		SetColor(ConsoleColor::LightGray, ConsoleColor::Black);
+		s = _getch();
+		cout << s << endl;
+
+		switch (s) {
+		case 'i': cout << "r - read\np - print\ns - save\nq - quit\ne - clear\nf - print free\nd - clear memory\n" <<
+			"a - go to previous link\nz - go to next link\nx - go to down link\n1 - insert next line\n2 - insert next section\n" <<
+			"3 - insert down line\n4 - insert down section\n5 - delete next link\n6 - delete down link\n"; break;
+		case 'r': text.Read(fileName);
+			if (text.GetpFirst() == NULL) { cout << "Reading error\n"; }
+			else { readFlag = true; cout << "Successful reading\n"; }; break;
+		case 'p': text.Print(); break;
+		case 's': text.Save("C:\\Users\\User\\Documents\\GitHub projects\\Lab6-Text_v2.0\\SavedText.txt"); cout << "Successful saving\n"; break;
+			//case 'c': TText tmp; tmp.Save("C:\\Users\\User\\Documents\\GitHub projects\\Lab6-Text_v2.0\\Copy.txt"); break;
+		case 'e': clrscr(); cout << "i - info\n"; break;
+		case 'f': TLink::PrintFree(); break;
+		case 'd': if (readFlag) { TLink::MemClear(text); cout << "Memory was cleared\n"; }
+				  else cout << "There is nothing to clear\n"; break;
+		case 'q': if (readFlag) { TLink::MemClear(text); } exit(0);
+		case 'a': text.GoPrevLink(); text.Print(); break;
+		case 'z': text.GoNextLink(); text.Print(); break;
+		case 'x': text.GoDownLink(); text.Print(); break;
+		case '1': cout << "Enter your line: "; cin >> buf; text.InsNextLine(buf); text.Print(); break;
+		case '2': cout << "Enter your line: "; cin >> buf; text.InsNextSection(buf); text.Print(); break;
+		case '3': cout << "Enter your line: "; cin >> buf; text.InsDownLine(buf); text.Print(); break;
+		case '4': cout << "Enter your line: "; cin >> buf; text.InsDownSection(buf); text.Print(); break;
+		case '5': text.DelNext(); text.Print(); break;
+		case '6': text.DelDown(); text.Print(); break;
+		default: cout << "Input error\n";
+		}
+	}
+
+	return 0;
+}
+
+
+
+
+void clrscr(void)
+{
+	HANDLE Console = GetStdHandle(STD_OUTPUT_HANDLE);
+	if (!Console) return;
+
+	CONSOLE_SCREEN_BUFFER_INFO buf;
+	GetConsoleScreenBufferInfo(Console, &buf);
+
+	DWORD Count;
+
+	COORD zpos;
+	zpos.X = 0;
+	zpos.Y = 0;
+
+	FillConsoleOutputCharacter(Console, ' ', buf.dwSize.X * buf.dwSize.Y, zpos, &Count);
+	SetConsoleCursorPosition(Console, zpos);
+}
+
+void clreol(void)
+{
+	HANDLE Console = GetStdHandle(STD_OUTPUT_HANDLE);
+	if (!Console)return;
+
+	CONSOLE_SCREEN_BUFFER_INFO buf;
+	GetConsoleScreenBufferInfo(Console, &buf);
+
+	WORD Attr;
+	DWORD Count;
+
+	COORD zpos = buf.dwCursorPosition;
+
+	zpos.X = buf.dwCursorPosition.X + 1;
+	zpos.Y = buf.dwCursorPosition.Y;
+
+	FillConsoleOutputCharacter(Console, ' ', buf.dwSize.X - buf.dwCursorPosition.X - 1, zpos, &Count);
+	SetConsoleCursorPosition(Console, buf.dwCursorPosition);
+}
+
+void gotoxy(int x, int y)
+{
+	HANDLE Console = GetStdHandle(STD_OUTPUT_HANDLE);
+	if (!Console)return;
+
+	COORD pos;
+	pos.X = x;
+	pos.Y = y;
+
+	SetConsoleCursorPosition(Console, pos);
+}
